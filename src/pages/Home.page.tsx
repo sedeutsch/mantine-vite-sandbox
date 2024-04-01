324cef458c96f77e42830af1d5dc647747cd7dd3
Stephen Deutsch <sedeutsch@gmail.com>
2024-03-31 20:33:54 -0400

    WIP

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,17 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { useDisclosure } from '@mantine/hooks';
+import { Drawer, Button, Container, Title, Text } from '@mantine/core';
 
-export function HomePage() {
+export const HomePage = () => {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
-    <>
-      <Welcome />
-      <ColorSchemeToggle />
-    </>
+    <Container mt="xl">
+      <Drawer opened={opened} position="right" onClose={close} title="Authentication">
+        <Title order={3}>Add assignments</Title>
+        <Text>Can you see this content inside the drawer?</Text>
+      </Drawer>
+
+      <Button onClick={open}>Open Drawer</Button>
+    </Container>
   );
-}
+};
